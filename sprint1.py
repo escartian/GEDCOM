bdf99b82cb77362c6c2df0cba8b198e6b101eee3
troydermond3296 <55443814+troydermond3296@users.noreply.github.com>
2024-06-16 07:20:12 -0400

    User Stories 1-3

--- sprint1.py
+++ sprint1.py
@@ -9,14 +9,108 @@ class sprint1:
 
         for i, line in enumerate(gedcom_lines):
             parts = line.strip().split()
-            if parts and parts[0] in ["1", "2"] and parts[1] in events:
+            if (parts and parts[0] in ["1", "2"] and parts[1] in events):
                 date_line = gedcom_lines[i + 1].strip()
                 date_parts = date_line.split()
-                if date_parts[0] == "2" and date_parts[1] == "DATE":
+                if (date_parts[0] == "2" and date_parts[1] == "DATE"):
                     try:
                         event_date = datetime.strptime(" ".join(date_parts[2:]), "%d %b %Y")
-                        if event_date > current_date:
+                        if (event_date > current_date):
                             invalid_dates.append((line, date_line))
                     except ValueError:
                         continue
         return invalid_dates
+        
+    def birth_before_marriage(self, gedcom_lines):
+        individuals = {}
+        current_id = None
+        birth_date = None
+        marriage_date = None
+
+        for i, line in enumerate(gedcom_lines):
+            parts = line.strip().split()
+            if parts:
+                if parts[0] == "0" and "@I" in parts[1]:
+                    if current_id and birth_date and marriage_date:
+                        individuals[current_id] = (birth_date, marriage_date)
+
+                    current_id = parts[1]
+                    birth_date = None
+                    marriage_date = None
+                elif parts[0] == "1":
+                    if parts[1] == "BIRT":
+                        date_line = gedcom_lines[i + 1].strip()
+                        date_parts = date_line.split()
+                        if date_parts[0] == "2" and date_parts[1] == "DATE":
+                            try:
+                                birth_date = datetime.strptime(" ".join(date_parts[2:]), "%d %b %Y")
+                            except ValueError:
+                                birth_date = None
+                    elif parts[1] == "MARR":
+                        date_line = gedcom_lines[i + 1].strip()
+                        date_parts = date_line.split()
+                        if date_parts[0] == "2" and date_parts[1] == "DATE":
+                            try:
+                                marriage_date = datetime.strptime(" ".join(date_parts[2:]), "%d %b %Y")
+                            except ValueError:
+                                marriage_date = None
+
+        # Add the last individual's dates
+        if current_id and birth_date and marriage_date:
+            individuals[current_id] = (birth_date, marriage_date)
+
+        print(individuals)
+
+        results = {}
+        for ind_id, (b_date, m_date) in individuals.items():
+            results[ind_id] = b_date < m_date if b_date and m_date else None
+            print(results)
+        
+        return results
+
+    def birth_before_death(self, gedcom_lines):
+        individuals = {}
+        current_id = None
+        birth_date = None
+        death_date = None
+
+        for i, line in enumerate(gedcom_lines):
+            parts = line.strip().split()
+            if parts:
+                if parts[0] == "0" and "@I" in parts[1]:
+                    if current_id and birth_date and death_date:
+                        individuals[current_id] = (birth_date, death_date)
+
+                    current_id = parts[1]
+                    birth_date = None
+                    death_date = None
+                elif parts[0] == "1":
+                    if parts[1] == "BIRT":
+                        date_line = gedcom_lines[i + 1].strip()
+                        date_parts = date_line.split()
+                        if date_parts[0] == "2" and date_parts[1] == "DATE":
+                            try:
+                                birth_date = datetime.strptime(" ".join(date_parts[2:]), "%d %b %Y")
+                            except ValueError:
+                                birth_date = None
+                    elif parts[1] == "DEAT":
+                        date_line = gedcom_lines[i + 1].strip()
+                        date_parts = date_line.split()
+                        if date_parts[0] == "2" and date_parts[1] == "DATE":
+                            try:
+                                death_date = datetime.strptime(" ".join(date_parts[2:]), "%d %b %Y")
+                            except ValueError:
+                                death_date = None
+
+        # Add the last individual's dates
+        if current_id and birth_date and death_date:
+            individuals[current_id] = (birth_date, death_date)
+
+        print(individuals)
+
+        results = {}
+        for ind_id, (b_date, m_date) in individuals.items():
+            results[ind_id] = b_date < m_date if b_date and m_date else None
+            print(results)
+        
+        return results
--- sprint1_test.py
+++ sprint1_test.py
@@ -92,5 +92,183 @@ class sprint1_test(unittest.TestCase):
         invalid_dates_5 = self.validator.dates_before_current_date(gedcom_lines_5)
         self.assertEqual(invalid_dates_5, expected_invalid_dates_5)
         
+    def test_birth_before_marriage(self):
+        gedcom_lines_1 = [
+           "0 @I1@ INDI",
+            "1 BIRT",
+            "2 DATE 1 JAN 1980",
+            "1 MARR",
+            "2 DATE 1 JAN 2000",
+            "0 @I2@ INDI",
+            "1 BIRT",
+            "2 DATE 25 DEC 1985",
+            "1 MARR",
+            "2 DATE 15 JUN 2010",
+            "0 @I3@ INDI",
+            "1 BIRT",
+            "2 DATE 15 MAR 1975",
+            "1 MARR",
+            "2 DATE 20 APR 1995",
+            "0 @I4@ INDI",
+            "1 BIRT",
+            "2 DATE 10 MAY 1990",
+            "1 MARR",
+            "2 DATE 11 MAY 1989",  
+            "0 @I5@ INDI",
+            "1 BIRT",
+            "2 DATE 20 JUN 1975",
+            "1 MARR",
+            "2 DATE 20 JUN 2025"
+        ]
+
+        gedcom_lines_2 = [
+            "0 @I2@ INDI",
+            "1 BIRT",
+            "2 DATE 25 DEC 1985",
+            "1 MARR",
+            "2 DATE 15 JUN 2010"
+        ]
+
+        gedcom_lines_3 = [
+            "0 @I3@ INDI",
+            "1 BIRT",
+            "2 DATE 20 MAR 1975",
+            "1 MARR",
+            "2 DATE 20 MAR 1975" #same day 
+        ]
+
+        gedcom_lines_4 = [
+            "0 @I4@ INDI",
+            "1 BIRT",
+            "2 DATE 10 MAY 1990",
+            "1 MARR",
+            "2 DATE 5 MAY 1989"  #invalid case
+        ]
+
+        gedcom_lines_5 = [
+            "0 @I5@ INDI",
+            "1 BIRT",
+            "2 DATE 20 JUN 1975",
+            "1 MARR",
+            "2 DATE 20 JUN 2025"
+        ]
+
+        expected_results_1 = {
+            "@I1@": True,
+            "@I2@": True,
+            "@I3@": True,
+            "@I4@": False,
+            "@I5@": True
+        }
+        expected_results_2 = {
+            "@I2@": True
+        }
+        expected_results_3 = {
+            "@I3@": False
+        }
+        expected_results_4 = {
+            "@I4@": False
+        }
+        expected_results_5 = {
+            "@I5@": True
+        }
+
+
+
+        self.assertEqual(self.validator.birth_before_marriage(gedcom_lines_1), expected_results_1)
+        self.assertEqual(self.validator.birth_before_marriage(gedcom_lines_2), expected_results_2)
+        self.assertEqual(self.validator.birth_before_marriage(gedcom_lines_3), expected_results_3)
+        self.assertEqual(self.validator.birth_before_marriage(gedcom_lines_4), expected_results_4)
+        self.assertEqual(self.validator.birth_before_marriage(gedcom_lines_5), expected_results_5)
+
+    def test_birth_before_death(self):
+        gedcom_lines_1 = [
+           "0 @I1@ INDI",
+            "1 BIRT",
+            "2 DATE 1 JAN 1980",
+            "1 DEAT",
+            "2 DATE 1 JAN 2000",
+            "0 @I2@ INDI",
+            "1 BIRT",
+            "2 DATE 25 DEC 1985",
+            "1 DEAT",
+            "2 DATE 15 JUN 2010",
+            "0 @I3@ INDI",
+            "1 BIRT",
+            "2 DATE 15 MAR 1975",
+            "1 DEAT",
+            "2 DATE 20 APR 1995",
+            "0 @I4@ INDI",
+            "1 BIRT",
+            "2 DATE 10 MAY 1990",
+            "1 DEAT",
+            "2 DATE 11 MAY 1989",  
+            "0 @I5@ INDI",
+            "1 BIRT",
+            "2 DATE 20 JUN 1975",
+            "1 DEAT",
+            "2 DATE 20 JUN 2025"
+        ]
+
+        gedcom_lines_2 = [
+            "0 @I2@ INDI",
+            "1 BIRT",
+            "2 DATE 25 DEC 1985",
+            "1 DEAT",
+            "2 DATE 15 JUN 2010"
+        ]
+
+        gedcom_lines_3 = [
+            "0 @I3@ INDI",
+            "1 BIRT",
+            "2 DATE 20 MAR 1975",
+            "1 DEAT",
+            "2 DATE 20 MAR 1975" #same day 
+        ]
+
+        gedcom_lines_4 = [
+            "0 @I4@ INDI",
+            "1 BIRT",
+            "2 DATE 10 MAY 1990",
+            "1 DEAT",
+            "2 DATE 5 MAY 1989"  #invalid case
+        ]
+
+        gedcom_lines_5 = [
+            "0 @I5@ INDI",
+            "1 BIRT",
+            "2 DATE 20 JUN 1975",
+            "1 DEAT",
+            "2 DATE 20 JUN 2025"
+        ]
+
+        expected_results_1 = {
+            "@I1@": True,
+            "@I2@": True,
+            "@I3@": True,
+            "@I4@": False,
+            "@I5@": True
+        }
+        expected_results_2 = {
+            "@I2@": True
+        }
+        expected_results_3 = {
+            "@I3@": False
+        }
+        expected_results_4 = {
+            "@I4@": False
+        }
+        expected_results_5 = {
+            "@I5@": True
+        }
+
+
+
+        self.assertEqual(self.validator.birth_before_death(gedcom_lines_1), expected_results_1)
+        self.assertEqual(self.validator.birth_before_death(gedcom_lines_2), expected_results_2)
+        self.assertEqual(self.validator.birth_before_death(gedcom_lines_3), expected_results_3)
+        self.assertEqual(self.validator.birth_before_death(gedcom_lines_4), expected_results_4)
+        self.assertEqual(self.validator.birth_before_death(gedcom_lines_5), expected_results_5)
+
 if __name__ == "__main__":
     unittest.main()
