79ad9b892098bf795d2f1cf6309e40a62ba9d82d
troydermond3296 <55443814+troydermond3296@users.noreply.github.com>
2024-06-17 20:17:50 -0400

    Add files via upload

--- sprint1.py
+++ sprint1.py
@@ -7,18 +7,23 @@ class sprint1:
         events = ["BIRT", "MARR", "DIV", "DEAT"]
         invalid_dates = []
 
+        #iterate through gedcom lines
         for i, line in enumerate(gedcom_lines):
             parts = line.strip().split()
+            #checking that the event is in the events array
             if (parts and parts[0] in ["1", "2"] and parts[1] in events):
                 date_line = gedcom_lines[i + 1].strip()
                 date_parts = date_line.split()
                 if (date_parts[0] == "2" and date_parts[1] == "DATE"):
                     try:
                         event_date = datetime.strptime(" ".join(date_parts[2:]), "%d %b %Y")
+                        # if event date is after current data 
                         if (event_date > current_date):
                             invalid_dates.append((line, date_line))
                     except ValueError:
                         continue
+
+        print (invalid_dates)
         return invalid_dates
         
     def birth_before_marriage(self, gedcom_lines):
@@ -27,6 +32,7 @@ class sprint1:
         birth_date = None
         marriage_date = None
 
+        
         for i, line in enumerate(gedcom_lines):
             parts = line.strip().split()
             if parts:
@@ -59,14 +65,13 @@ class sprint1:
         if current_id and birth_date and marriage_date:
             individuals[current_id] = (birth_date, marriage_date)
 
-        print(individuals)
+        #print(individuals)
 
         results = {}
         for ind_id, (b_date, m_date) in individuals.items():
             results[ind_id] = b_date < m_date if b_date and m_date else None
             print(results)
         
-        return results
 
     def birth_before_death(self, gedcom_lines):
         individuals = {}
@@ -106,7 +111,7 @@ class sprint1:
         if current_id and birth_date and death_date:
             individuals[current_id] = (birth_date, death_date)
 
-        print(individuals)
+        #print(individuals)
 
         results = {}
         for ind_id, (b_date, m_date) in individuals.items():
